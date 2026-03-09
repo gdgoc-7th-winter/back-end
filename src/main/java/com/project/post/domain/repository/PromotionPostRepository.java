4880e5764b8325cde7cb57bb92a9e08a19212e9d
jueun036 <jueun036@gmail.com>
2026-03-10 01:50:11 +0900

    feat: 홍보글 목록 조회 API 구현

--- src/main/java/com/project/post/domain/repository/PromotionPostRepository.java
+++ src/main/java/com/project/post/domain/repository/PromotionPostRepository.java
@@ -1,7 +1,15 @@
 package com.project.post.domain.repository;
 
 import com.project.post.domain.entity.PromotionPost;
+import com.project.post.domain.enums.PromotionCategory;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PromotionPostRepository extends JpaRepository<PromotionPost, Long> {
+
+    Page<PromotionPost> findByCategory(PromotionCategory category, Pageable pageable);
+    List<PromotionPost> findByCategory(PromotionCategory category);
+
 }
