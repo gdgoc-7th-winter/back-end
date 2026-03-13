227d16234ec47c2c79828b7d6d72fc11739fec27
Trashbin4943 <anthonyhwan415@gmail.com>
2026-03-13 16:52:27 +0900

    feat: Contribution 점수 부여 및 사용자 레벨 연동

--- src/test/java/com/project/user/application/service/EmailServiceTest.java
+++ src/test/java/com/project/user/application/service/EmailServiceTest.java
@@ -1,4 +1,4 @@
-package com.project.user;
+package com.project.user.application.service;
 
 import com.project.user.application.service.impl.EmailServiceImpl;
 import org.junit.jupiter.api.AfterEach;
@@ -19,10 +19,10 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class EmailServiceImplTest {
+class EmailServiceTest {
 
     @Autowired
-    private EmailServiceImpl emailServiceImpl;
+    private EmailServiceImpl emailService;
 
     @MockitoBean
     private JavaMailSender mailSender;
@@ -48,7 +48,7 @@ class EmailServiceImplTest {
     @DisplayName("실제 로직을 실행하여 Redis에 6자리 난수가 저장되는지 확인한다")
     void sendEmailAndCheckRedis() {
         // 1. 발송 로직 실행 (실제 메일 발송 포함)
-        emailServiceImpl.sendAuthEmail(testEmail);
+        emailService.sendAuthEmail(testEmail);
 
         // 2. Redis에서 직접 데이터 꺼내기
         String savedCode = redisTemplate.opsForValue().get("AUTH_CODE:" + testEmail);
@@ -62,7 +62,7 @@ class EmailServiceImplTest {
     @Test
     @DisplayName("Redis 키와 값을 직접 출력하며 확인")
     void checkAuthCodeTTL() {
-        emailServiceImpl.sendAuthEmail(testEmail);
+        emailService.sendAuthEmail(testEmail);
         String targetKey = "AUTH_CODE:" + testEmail;
         Long expireTime = redisTemplate.getExpire(targetKey, TimeUnit.SECONDS);
         assertThat(expireTime).isGreaterThan(290L);
