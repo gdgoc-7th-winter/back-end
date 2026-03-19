78a53f93f0de16d4e2b902332bd87ab8a76e0ee7
1000hyehyang <ducogus12@gmail.com>
2026-03-20 01:36:50 +0900

    feat: S3, CloudFront 설정 클래스 추가

---
+++ src/main/java/com/project/file/config/S3ClientConfig.java
@@ -0,0 +1,21 @@
+package com.project.file.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3ClientConfig {
+
+    @Bean
+    public S3Client s3Client(S3Properties s3Properties) {
+        return S3Client.builder()
+                .region(Region.of(s3Properties.getRegion()))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(s3Properties.getAccessKeyId(), s3Properties.getSecretAccessKey())))
+                .build();
+    }
+}
