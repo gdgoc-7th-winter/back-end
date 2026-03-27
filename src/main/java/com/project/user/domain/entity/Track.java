2695ce7a9abbbd452d3b5a90f441e2660e602a9a
Trashbin4943 <anthonyhwan415@gmail.com>
2026-03-27 20:44:03 +0900

    fix: Interceptor 적용대상에서 프로필 조회 (GET) 페이지 예외대상 추가

--- src/main/java/com/project/user/domain/entity/Track.java
+++ src/main/java/com/project/user/domain/entity/Track.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Column;
+import jakarta.persistence.Table;
 
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -13,6 +14,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity(name="tracks")
+@Table
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Track {
