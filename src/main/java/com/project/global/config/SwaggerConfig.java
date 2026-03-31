b4b6f2f8de656ed836b922bf893a006555f7cdea
1000hyehyang <ducogus12@gmail.com>
2026-04-01 05:02:55 +0900

    feat: swagger에 X-XSRF-TOKEN 스키마 연결

--- src/main/java/com/project/global/config/SwaggerConfig.java
+++ src/main/java/com/project/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.project.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
 import org.springdoc.core.models.GroupedOpenApi;
@@ -19,7 +22,14 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Backend API")
                         .description("한국외대 개발자 커뮤니티 백엔드 API")
-                        .version("v1.0.0"));
+                        .version("v1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes("csrf-token", new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("X-XSRF-TOKEN")
+                                .description("브라우저 Application → Cookies → `XSRF-TOKEN` 값을 복사해 입력")))
+                .addSecurityItem(new SecurityRequirement().addList("csrf-token"));
         if (StringUtils.hasText(serverUrl)) {
             api.setServers(List.of(new Server().url(serverUrl.trim()).description("API")));
         }
