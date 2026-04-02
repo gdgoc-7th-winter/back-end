8ab56e03b4d84a7752a4503c23ea3184d1c71d34
jueun036 <jueun036@gmail.com>
2026-04-02 23:52:48 +0900

    refactor: 지원한 모집글 목록 조회를 QueryDSL 기반으로 변경

--- src/main/java/com/project/post/domain/repository/ApplicationSubmissionRepository.java
+++ src/main/java/com/project/post/domain/repository/ApplicationSubmissionRepository.java
@@ -2,21 +2,17 @@ package com.project.post.domain.repository;
 
 import com.project.post.domain.entity.ApplicationSubmission;
 import com.project.post.domain.entity.RecruitingApplication;
-import com.project.post.domain.repository.dto.AppliedRecruitingPostListQueryResult;
 import com.project.user.domain.entity.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ApplicationSubmissionRepository
         extends JpaRepository<ApplicationSubmission, Long>,
-        JpaSpecificationExecutor<ApplicationSubmission> {
+        JpaSpecificationExecutor<ApplicationSubmission>,
+        ApplicationSubmissionRepositoryCustom {
 
     boolean existsByRecruitingApplicationAndUserAndDeletedAtIsNull(
             RecruitingApplication recruitingApplication,
@@ -34,54 +30,4 @@ public interface ApplicationSubmissionRepository
     List<ApplicationSubmission> findAllByUserAndDeletedAtIsNullOrderBySubmittedAtDesc(User user);
 
     Optional<ApplicationSubmission> findByIdAndDeletedAtIsNull(Long id);
-
-    @Query(value = """
-        select new com.project.post.domain.repository.dto.AppliedRecruitingPostListQueryResult(
-            s.id,
-            rp.category,
-            rp.startedAt,
-            rp.deadlineAt,
-            s.submittedAt,
-            p.id,
-            p.title,
-            p.thumbnailUrl,
-            author.id,
-            author.nickname,
-            author.profileImgUrl,
-            dept.name,
-            null,
-            lb.levelImage,
-            case when author.deletedAt is null then false else true end,
-            p.viewCount,
-            p.likeCount,
-            p.scrapCount,
-            p.commentCount,
-            p.createdAt
-        )
-        from ApplicationSubmission s
-        join s.recruitingApplication ra
-        join ra.recruitingPost rp
-        join rp.post p
-        join p.author author
-        left join author.department dept
-        left join author.levelBadge lb
-        where s.user.id = :userId
-          and s.deletedAt is null
-          and p.deletedAt is null
-        """,
-            countQuery = """
-        select count(s)
-        from ApplicationSubmission s
-        join s.recruitingApplication ra
-        join ra.recruitingPost rp
-        join rp.post p
-        where s.user.id = :userId
-          and s.deletedAt is null
-          and p.deletedAt is null
-        """
-    )
-    Page<AppliedRecruitingPostListQueryResult> findAppliedRecruitingPostListByUserId(
-            @Param("userId") Long userId,
-            Pageable pageable
-    );
 }
